86a78d29904da8aac31dd1183af1812d385268c5
maggie weir <mlweir98@gmail.com>
2023-05-02 11:25:35 -0400

    migrations

---
+++ migrations/versions/37e0a2f31f93_merge_migrations.py
@@ -0,0 +1,24 @@
+"""merge migrations
+
+Revision ID: 37e0a2f31f93
+Revises: 2f24b51fc557
+Create Date: 2023-04-28 11:39:39.181634
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '37e0a2f31f93'
+down_revision = '2f24b51fc557'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
